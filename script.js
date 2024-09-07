c66aeeb52daf02f2ff5607736ff27f1e8bf58fbb
Grunt395 <grantlew@att.net>
2024-09-06 19:08:22 -0700

    Separated into functions and closing modal

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnsShowModal.length; i++) {
-    console.log(btnsShowModal[i].textContent);
+// Function for closing modal
+function closeModal() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
 }
+
+// Function for opening modal
+function openModal() {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+}
+
+// Loop for selecting which button from querySelectorAll
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener("click", openModal);
+}
+
+// Closing Modal by clicking surrounding overlay
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
