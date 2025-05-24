1ba40d7407fade5cdcb4e18b33ae3072b007c9aa
Grant Lewandowski <grantlew@att.net>
2025-05-24 15:18:35 -0700

    Added opening and closing modal

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const modal = document.querySelector(".modal");
+const modalButton = document.querySelector(".show-modal");
+const closeModal = document.querySelector(".close-modal");
+
+modalButton.addEventListener("click", function () {
+    modal.classList.remove("hidden");
+});
+
+closeModal.addEventListener("click", function () {
+    modal.classList.add("hidden");
+});
