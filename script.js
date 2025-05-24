e4b0f33e50fb77c8e578eb4dc7de6db7b1dd90c2
Grant Lewandowski <grantlew@att.net>
2025-05-24 15:32:50 -0700

    QuerySelectorAll added with loop

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const modal = document.querySelector(".modal");
-const modalButton = document.querySelector(".show-modal");
+const overlay = document.querySelector(".overlay");
+const openModal = document.querySelectorAll(".show-modal");
 const closeModal = document.querySelector(".close-modal");
 
-modalButton.addEventListener("click", function () {
-    modal.classList.remove("hidden");
-});
+for (let i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener("click", function () {
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+        // console.log(`Button ${i + 1} clicked`);
+    });
+}
 
 closeModal.addEventListener("click", function () {
     modal.classList.add("hidden");
+    overlay.classList.add("hidden");
 });
