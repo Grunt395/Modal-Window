299564c87431b025a7dcbb213d97744f51b6635f
Grant Lewandowski <grantlew@att.net>
2025-05-24 15:44:58 -0700

    Created closeModal function

--- script.js
+++ script.js
@@ -2,18 +2,21 @@
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const openModal = document.querySelectorAll(".show-modal");
-const closeModal = document.querySelector(".close-modal");
+const modalOpenButton = document.querySelectorAll(".show-modal");
+const modalCloseButton = document.querySelector(".close-modal");
 
-for (let i = 0; i < openModal.length; i++) {
-    openModal[i].addEventListener("click", function () {
+for (let i = 0; i < modalOpenButton.length; i++) {
+    modalOpenButton[i].addEventListener("click", function () {
         modal.classList.remove("hidden");
         overlay.classList.remove("hidden");
         // console.log(`Button ${i + 1} clicked`);
     });
 }
 
-closeModal.addEventListener("click", function () {
+function closeModal() {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
-});
+}
+
+modalCloseButton.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
